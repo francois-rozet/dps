189b69d30682b7406467c3bcbe7e3d6a1fb41da8
François Rozet <francois.rozet@outlook.com>
2024-08-05 14:01:01 +0200

    Refactor repository

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -5,18 +5,15 @@ do
     for steps in 0010 0100 1000
     do
         # DPS
-        python sample_condition.py --task_config ./configs/tasks/${task}_config.yaml --method ps --scale 0.5 --steps $steps --basename ${task}_dps_${steps} --slurm &
+        python sample.py --task_config ./configs/tasks/${task}_config.yaml --method dps --scale 0.5 --steps $steps --basename ${task}_dps_${steps} --slurm &
 
         # PGDM
-        for maxiter in 01 16
-        do
-            python sample_condition.py --task_config ./configs/tasks/${task}_config.yaml --method pgdm --maxiter $maxiter --steps $steps --basename ${task}_pgdm_${steps}_${maxiter} --slurm &
-        done
+        python sample.py --task_config ./configs/tasks/${task}_config.yaml --method pgdm --maxiter 5 --steps $steps --basename ${task}_pgdm_${steps} --slurm &
 
         # MMPS
         for maxiter in 01 05
         do
-            python sample_condition.py --task_config ./configs/tasks/${task}_config.yaml --method mmps --maxiter $maxiter --steps $steps --basename ${task}_mmps_${steps}_${maxiter} --slurm &
+            python sample.py --task_config ./configs/tasks/${task}_config.yaml --method mmps --maxiter $maxiter --steps $steps --basename ${task}_mmps_${steps}_${maxiter} --slurm &
         done
     done
 done
